cc470a8f1b6bd91263eab322a72fa833f39cadb3
elou <elou@watch.lan>
2023-09-12 20:51:32 +0200

    Fixing issue with package build and .gitignore

--- build_package.sh
+++ build_package.sh
@@ -7,12 +7,16 @@ read chat
 echo -n "Which hour it triggers [HH:MM:SS] : "
 read hour
 
-sed -E "s/%BOT_TOKEN%/$bot/;s/%CHAT_ID%/$chat/;s/%HOUR%/$hour/" ./pLUM > ./pLUM-package/usr/local/bin/pLUM
+/usr/bin/mkdir -p ./pLUM-package/usr/local/bin/
 
-chmod 700 ./pLUM-package/usr/local/bin/pLUM
+/usr/bin/sed -E "s/%BOT_TOKEN%/$bot/;s/%CHAT_ID%/$chat/;s/%HOUR%/$hour/" ./pLUM > ./pLUM-package/usr/local/bin/pLUM
 
-sed -E "s/%HOUR%/$hour/" ./pLUM.timer > ./pLUM-package/usr/lib/systemd/system/pLUM.timer
+/usr/bin/chmod 700 ./pLUM-package/usr/local/bin/pLUM
+
+/usr/bin/mkdir -p ./pLUM-package/usr/lib/systemd/system/
+
+/usr/bin/sed -E "s/%HOUR%/$hour/" ./pLUM.timer > ./pLUM-package/usr/lib/systemd/system/pLUM.timer
 
 /usr/bin/cp ./pLUM.service ./pLUM-package/usr/lib/systemd/system/
 
-dpkg-deb --build ./pLUM-package/
+/usr/bin/dpkg-deb --build ./pLUM-package/
